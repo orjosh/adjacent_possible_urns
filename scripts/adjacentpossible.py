a7f0ac4c2f494ae309dd21f75cfa1901fa1cd146
orjosh <orjosh@gmail.com>
2024-02-04 08:40:07 +1100

    Fixed a bug where few new urns were being created

--- scripts/adjacentpossible.py
+++ scripts/adjacentpossible.py
@@ -2,7 +2,8 @@ from typing import Dict
 from dataclasses import dataclass
 import random
 import numpy as np
-from datautils import choose_proportional
+from math import floor
+from datautils import choose_proportional, choose_proportional_lazy
 
 @dataclass
 class UserUrn:
@@ -12,15 +13,20 @@ class UserUrn:
     contacts: Dict # (K,V) = (ID, count)
     n_contacts: int = 0
 
-    def add_contact(self, urn):
-        if self.contacts is []:
-            self.contacts = {urn.ID: 1}
-        elif urn.ID in self.contacts:
-            self.contacts[urn.ID] += 1
+    def add_contact(self, urn_id):
+        # print(f"Adding {urn_id} to {self.ID}")
+        if not self.contacts:
+            # print("empty")
+            self.contacts = {urn_id: 1}
+            self.n_contacts += 1
+        elif urn_id in self.contacts:
+            # not a new contact
+            self.contacts[urn_id] += 1
         else:
-            self.contacts[urn.ID] = 1
-
-        self.n_contacts += 1
+            # print(f"{urn_id} not in {self.contacts}")
+            self.contacts[urn_id] = 1
+            self.n_contacts += 1
+            # print(f"is now {self.contacts}")
 
     def extract_prop(self):
         freqs = []
@@ -55,43 +61,39 @@ class AdjPosModel:
 
         random.seed(rng_seed)
 
-    def _get_calling_urn(self):
+    def _get_calling_urn(self, lazy=False):
         urn_sizes = []
         for u in self.urns:
-            urn_sizes.append(u.n_contacts)
+            urn_sizes.append(u.n_contacts) # NOTE: don't +1 to these; shouldn't select empty urns
 
         return choose_proportional(self.urns, urn_sizes, self.n_urns)
 
-    def time_step(self):
+    def time_step(self, lazy=False):
         """
         Performs a single time step of the model according to the set strategy (WSW, by default).
         NOTE: Expects at least one urn to have a non-empty contacts dict
         """
         # get caller and receiver, store event
-        caller = self._get_calling_urn()
+        caller = self._get_calling_urn(lazy)
+        # print(f"CALLER IS {caller}")
         unique_contact_ids = list(caller.contacts.keys())
         unique_contact_counts = list(caller.contacts.values())
-
-        for i, x in enumerate(unique_contact_counts):
-            # we do this so that empty urns can be selected as receivers too
-            unique_contact_counts[i] += 1
 
         receiver_id = choose_proportional(unique_contact_ids, unique_contact_counts, caller.n_contacts)
         receiver = None
         for urn in self.urns:
             if urn.ID == receiver_id:
                 receiver = urn
-        self.events.append((caller.ID, receiver.ID))
-        #print(self.events)
+                break
 
         # reinforcement step
         receiver_is_empty = False
-        caller.contacts[receiver.ID] += self.reinforcement_param
-        if len(receiver.contacts) == 0: # receiver may be empty
+        if receiver.n_contacts == 0: # so we can do novelty step
             receiver_is_empty = True
-            receiver.contacts[caller.ID] = self.reinforcement_param
-        else:
-            receiver.contacts[caller.ID] += self.reinforcement_param
+
+        for i in range(self.reinforcement_param):
+            caller.add_contact(receiver.ID)
+            receiver.add_contact(caller.ID)
 
         # create v + 1 new nodes if receiver was empty
         if receiver_is_empty:
@@ -99,23 +101,100 @@ class AdjPosModel:
                 new_urn = UserUrn(len(self.urns) + 1, {})
                 self.urns.append(new_urn)
                 self.n_urns += 1
-                receiver.contacts[new_urn.ID] = 1
+                receiver.add_contact(new_urn.ID)
+                # print(f"Added {new_urn.ID} to {receiver.ID}")
 
         # novelty step, strategy called here
+        # print(f"looking for ({caller.ID}, {receiver.ID}) in {self.events}")
         if (caller.ID, receiver.ID) not in self.events \
             and (receiver.ID, caller.ID) not in self.events: # TODO not efficient, replace with interaction matrix version
             # novelty only occurs if urns never interacted before
+            # print(f"{caller.ID} and {receiver.ID} never interacted bvefore")
             if self.strategy == "WSW":
-                # TODO ids and counts of caller previously calculated (unique_contact_...), no point re-calculating?
                 self._do_WSW(caller, receiver)
             else:
                 raise ValueError(f"'{self.strategy}' is not a valid strategy.")
 
+        self.events.append((caller.ID, receiver.ID))
+
     def _do_WSW(self, caller, receiver):
+        # print(f"WSW with C {caller.ID} R {receiver.ID}")
+        # print(f"\tC: {caller.contacts}\n\tR: {receiver.contacts}")
+        # NOTE we copy all these things as they are now, so that novel urns which
+        # are about to be added cannot be chosen accidentally
+        caller_ids = list(caller.contacts.keys())
+        receiver_ids = list(receiver.contacts.keys())
+
+        caller_contacts = caller.contacts.copy()
+        caller_len = caller.n_contacts
+        receiver_contacts = receiver.contacts.copy()
+        receiver_len = receiver.n_contacts
+
+        # Choose v+1 unique IDs from caller, add to receiver
+        num_iter = self.novelty_param + 1
+        novel_options = list(set(caller_contacts.keys()) - set(receiver_contacts.keys())) # complement
+        # print(f"C - R = {novel_options}")
+        if len(novel_options) == 1:
+            # TODO hacky fix, is something broken? Without this, sometimes the only novelty to give an urn is itself
+            num_iter = 0
+        elif len(novel_options) - 1 < self.novelty_param + 1:
+            # Supplementary notes say that we pick only v distinct IDs if urn i
+            # would otherwise have to give a copy of urn j to urn j itself
+            num_iter -= 1
+        
+
+        for i in range(num_iter):
+            drawn_id = receiver.ID
+            while drawn_id is receiver.ID or drawn_id in list(receiver_contacts.keys()):
+                # print("C drawing")
+                # print(f"\t iter {i}/{num_iter}")
+                # print(f"\tR ({receiver.ID}): {receiver_ids}\n\tC ({caller.ID}): {caller_ids}")
+                # print(f"\tComplement: {list(set(caller_ids) - set(receiver_ids))}")
+                drawn_id = choose_proportional(list(caller_contacts.keys()), \
+                    list(caller_contacts.values()), caller_len)
+            # otherwise,
+            receiver.add_contact(drawn_id)
+            caller_contacts.pop(drawn_id)
+            caller_len -= 1
+
+        # The same but from receiver, adding to caller
+        caller_contacts = caller.contacts.copy()
+        num_iter = self.novelty_param + 1
+        novel_options = list(set(receiver_contacts.keys()) - set(caller_contacts.keys())) # complement of both sets
+        # print(f"R - C = {novel_options}")
+        if len(novel_options) == 1:
+            # TODO hacky fix, see above
+            num_iter = 0
+        elif len(novel_options) - 1 < self.novelty_param + 1:
+            # Supplementary notes say that we pick only v distinct IDs if urn i
+            # would otherwise have to give a copy of urn j to urn j itself
+            num_iter -= 1
+        
+
+        for i in range(num_iter):
+            drawn_id = receiver.ID
+            while drawn_id is caller.ID or drawn_id in list(caller_contacts.keys()):
+                # print("R drawing")
+                # print(f"\t iter {i}/{num_iter}")
+                # print(f"\tR ({receiver.ID}): {receiver_ids}\n\tC ({caller.ID}): {caller_ids}")
+                # print(f"\tComplement: {novel_options}")
+                drawn_id = choose_proportional(list(receiver_contacts.keys()), \
+                    list(receiver_contacts.values()), receiver_len)
+            # otherwise,
+            caller.add_contact(drawn_id)
+            receiver_contacts.pop(drawn_id)
+            receiver_len -= 1
+
+    def _do_WSW_old(self, caller, receiver):
+        # print(f"WSW with C {caller} R {receiver}")
+        # NOTE we copy all these things as they are now, so that novel urns which
+        # are about to be added cannot be chosen accidentally
         caller_ids = list(caller.contacts.keys())
         caller_counts = list(caller.contacts.values())
+        caller_len = caller.n_contacts
         receiver_ids = list(receiver.contacts.keys())
         receiver_counts = list(receiver.contacts.values())
+        receiver_len = receiver.n_contacts
 
         # choose v + 1 unique IDs proportionally from caller, add to receiver
         already_chosen = []
@@ -130,11 +209,12 @@ class AdjPosModel:
                 or drawn_id in receiver_ids:
                 # must pick a unique ID each time, and also can't give an
                 # urn its own ID
-                drawn_id = choose_proportional(caller_ids, caller_counts, caller.n_contacts)
-            if drawn_id in receiver.contacts:
-                receiver.contacts[drawn_id] += 1
-            else:
-                receiver.contacts[drawn_id] = 1
+                print(f"C{i}: {caller_ids} with sizes {caller_counts} and length {caller_len}")
+                print(f"R has {receiver_ids}, already chose {already_chosen}")
+                drawn_id = choose_proportional(caller_ids, caller_counts, caller_len)
+                print(f"Drew {drawn_id}")
+
+            receiver.add_contact(drawn_id)
             already_chosen.append(drawn_id)
 
         # vice versa
@@ -148,9 +228,8 @@ class AdjPosModel:
             drawn_id = caller.ID
             while drawn_id == caller.ID or drawn_id in already_chosen \
                 or drawn_id in caller_ids:
-                drawn_id = choose_proportional(receiver_ids, receiver_counts, receiver.n_contacts)
-            if drawn_id in caller.contacts:
-                caller.contacts[drawn_id] += 1
-            else:
-                caller.contacts[drawn_id] = 1
+                print(f"R{i}: {receiver_ids} with sizes {receiver_counts} and length {receiver_len}")
+                drawn_id = choose_proportional(receiver_ids, receiver_counts, receiver_len)
+
+            caller.add_contact(drawn_id)
             already_chosen.append(drawn_id)
--- scripts/datautils.py
+++ scripts/datautils.py
@@ -1,5 +1,8 @@
 import numpy as np
 import random
+import concurrent.futures
+import functools
+from math import floor
 
 def power_law_distributed_rng(n, x_min, scale, seed=None):
     # This algorithm is taken from Appendix D in Clauset et al's"Power Law Distributions
@@ -10,6 +13,38 @@ def power_law_distributed_rng(n, x_min, scale, seed=None):
         r = sample[i]
         sample[i] = x_min * (1-r)**(-1/(scale-1))
     return sorted(sample)
+
+def choose_proportional_lazy(old, n, ls, sizes, n_sizes):
+    '''
+    Randomly chooses and returns an element from the list `ls` proportional to its
+    size as determined by the corresponding index in `sizes`. In other words, draw
+    an element of `ls` out of a hat, where each `ls[i]` has `sizes[i]` entries
+    in the draw.
+
+    NOTE: `ls` and `sizes` must be one-to-one, i.e. `ls[i]` has a size of `sizes[i]`
+    '''
+    total_size = 0
+    for val in sizes:
+        total_size += val
+
+    if total_size == 0:
+        raise ValueError("At least one element in `sizes` must be non-zero.")
+
+    proportions = old
+    _calc_proportions_inplace(proportions, (0, n), sizes, total_size)
+
+    r = random.random() # btwn 0 and 1
+    # print("after:")
+    # print(proportions)
+    # print(f"random: {r}")
+    bin_start = 0.0
+    bin_end = 0.0
+    for i in range(n_sizes):
+        bin_end += proportions[i]
+        if r >= bin_start and r <= bin_end:
+            # print(f"returning {ls[i]}")
+            return (ls[i], proportions)
+        bin_start = bin_end
 
 def choose_proportional(ls, sizes, n_sizes):
     '''
@@ -27,15 +62,74 @@ def choose_proportional(ls, sizes, n_sizes):
     if total_size == 0:
         raise ValueError("At least one element in `sizes` must be non-zero.")
 
-    proportions = sizes
-    for i in range(n_sizes):
-        proportions[i] = proportions[i] / total_size
+    proportions = sizes.copy()
+    _calc_proportions_inplace(proportions, (0, n_sizes-1), sizes, total_size)
 
     r = random.random() # btwn 0 and 1
+    # print("after:")
+    # print(proportions)
+    # print(f"random: {r}")
     bin_start = 0.0
     bin_end = 0.0
     for i in range(n_sizes):
         bin_end += proportions[i]
         if r >= bin_start and r <= bin_end:
+            # print(f"returning {ls[i]}")
             return ls[i]
-        bin_start = bin_end 
+        bin_start = bin_end
+
+def choose_proportional_multithread(ls, sizes, n_sizes, max_workers=1):
+    '''
+    Randomly chooses and returns an element from the list `ls` proportional to its
+    size as determined by the corresponding index in `sizes`. In other words, draw
+    an element of `ls` out of a hat, where each `ls[i]` has `sizes[i]` entries
+    in the draw.
+
+    NOTE: `ls` and `sizes` must be one-to-one, i.e. `ls[i]` has a size of `sizes[i]`
+    '''
+    total_size = 0
+    for val in sizes:
+        total_size += val
+
+    if total_size == 0:
+        raise ValueError("At least one element in `sizes` must be non-zero.")
+
+    ranges = []
+    range_size = floor(n_sizes/max_workers)
+    rem = n_sizes % max_workers
+    for i in range(max_workers):
+        ranges.append((i*range_size, (i+1)*range_size-1))
+    if n_sizes % max_workers != 0:
+        ranges.append((max_workers*range_size, max_workers*range_size+rem-1))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        calc_prop = functools.partial(_calc_proportions, sizes=sizes, total_size=total_size)
+        results = executor.map(calc_prop, ranges)
+
+    proportions = []
+    for r in list(results):
+        proportions += r
+
+    r = random.random() # btwn 0 and 1
+    # print("after:")
+    # print(proportions)
+    # print(f"random: {r}")
+    bin_start = 0.0
+    bin_end = 0.0
+    for i in range(n_sizes):
+        bin_end += proportions[i]
+        if r >= bin_start and r <= bin_end:
+            # print(f"returning {ls[i]}")
+            return ls[i]
+        bin_start = bin_end
+
+def _calc_proportions(index_range, sizes, total_size):
+    proportions = []
+    for i in range(index_range[0], index_range[1]+1):
+        proportions.append(sizes[i] / total_size)
+    
+    return proportions
+
+def _calc_proportions_inplace(ls, index_range, sizes, total_size):
+    for i in range(index_range[0], index_range[1]+1):
+        ls[i] = sizes[i] / total_size
